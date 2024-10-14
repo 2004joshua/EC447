c96fe53b80cf3be83b58e00be5775c61d46f39de
2004joshua <157152052+2004joshua@users.noreply.github.com>
2024-10-13 22:08:27 -0400

    Add files via upload

---
+++ lab_2/lab_2/Lab_2.Designer.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Forms; 
+
+namespace Lab_2
+{
+    partial class Lab_2
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button button1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new Button();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Joshua Arrevillaga - Lab 2";
+
+            // Button Configuration
+            this.button1.Location = new System.Drawing.Point(10, 10);
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.Text = "Align";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Controls.Add(this.button1);
+
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        #endregion
+    }
+}
+
---
+++ lab_2/lab_2/Lab_2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Lab_2 : Form
+    {
+        private ArrayList coordinates = new ArrayList();
+        private ArrayList grid = new ArrayList();
+        private bool _IsOn;
+
+        public Lab_2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private int Gridround(int point)
+        {
+            int x = point % 40;
+            return (x < 20) ? point - x : point + (40 - x);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point original = new Point(e.X, e.Y);
+                Point aligned = new Point(Gridround(e.X), Gridround(e.Y));
+                coordinates.Add(original);
+                grid.Add(aligned);
+                Invalidate();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                coordinates.Clear();
+                grid.Clear();
+                Invalidate();
+            }
+        }
+
+        public bool IsOn
+        {
+            get => _IsOn;
+            set
+            {
+                _IsOn = value;
+                button1.Text = _IsOn ? "Original" : "Align";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IsOn = !IsOn;
+            Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.PageUnit = GraphicsUnit.Pixel;
+
+            // Draw Grid
+            int GRID = 40;
+            for (int x = GRID; x < ClientRectangle.Width; x += GRID)
+                g.DrawLine(Pens.Black, x, 0, x, ClientRectangle.Height);
+            for (int y = GRID; y < ClientRectangle.Height; y += GRID)
+                g.DrawLine(Pens.Black, 0, y, ClientRectangle.Width, y);
+
+            // Draw Circles
+            ArrayList activeList = _IsOn ? grid : coordinates;
+            foreach (Point p in activeList)
+            {
+                g.FillEllipse(Brushes.Red, p.X - 10, p.Y - 10, 20, 20);
+            }
+        }
+    }
+}
